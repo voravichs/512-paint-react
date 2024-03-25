6784d957e90b8106b9cf266cff7ba5a879acc8e6
VoravichS <vsilapac@gmail.com>
2024-03-25 19:18:41 -0400

    readme and deployment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 512-paint-react
+
+This is a React implementation for Lab 3 in CIS 4120/5120. It is a basic paint app used to demonstrate basic usage of React for UI/UX for web development.
+
+![website screenshot](./screenshot.PNG)
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useRef, useEffect } from "react";
+import { FaPencil, FaEraser } from "react-icons/fa6";
+
 
 function App() {
   // States
+  const [draw, setDraw] = useState(true)
   const [red, setRed] = useState(0)
   const [green, setGreen] = useState(0)
   const [blue, setBlue] = useState(0)
@@ -16,12 +19,24 @@ function App() {
     const context = canvas.getContext('2d')
     let isDragging = false;
 
-    canvas.addEventListener("mousedown", function (event) {
-      isDragging = true;
-      context.strokeStyle = `rgb(${red} ${green} ${blue})`;
-      context.beginPath();
-      context.moveTo(event.offsetX, event.offsetY)
-    })
+    if (draw) {
+      canvas.addEventListener("mousedown", function (event) {
+        isDragging = true;
+        context.lineWidth = 5;
+        context.strokeStyle = `rgb(${red} ${green} ${blue})`;
+        context.beginPath();
+        context.moveTo(event.offsetX, event.offsetY)
+      })
+    } else {
+      canvas.addEventListener("mousedown", function (event) {
+        isDragging = true;
+        context.lineWidth = 15;
+        context.strokeStyle = `rgb(255 255 255)`;
+        context.beginPath();
+        context.moveTo(event.offsetX, event.offsetY)
+      })
+    }
+
 
     canvas.addEventListener("mousemove", function (event) {
       if (isDragging) {
@@ -33,7 +48,7 @@ function App() {
     canvas.addEventListener("mouseup", function () {
       isDragging = false;
     })
-  }, [red, green, blue])
+  }, [red, green, blue, draw])
 
   // Color Input
   const handleColorChange = (event) => {
@@ -98,30 +113,56 @@ function App() {
         </h1>
         <div className="container mx-auto flex gap-8">
           <div className="mx-auto mb-8 flex flex-col gap-4 items-center">
-            <input type="color" className="p-1 ring-2 rounded " value={color} onChange={handleColorPicker} />
+            <div className="flex gap-4">
+              <input type="color" className="p-1 ring-2 rounded h-12 w-24" value={color} onChange={handleColorPicker} />
+              {draw
+                ?
+                <button
+                  className="px-4 py-1 w-16 flex-center ring-2 rounded bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                  <FaPencil className="text-white" />
+                </button>
+                :
+                <button onClick={() => setDraw(true)}
+                  className="px-4 py-1 w-16 flex-center ring-2 rounded">
+                  <FaPencil />
+                </button>
+              }
+              {!draw
+                ?
+                <button className="px-4 py-1 w-16 flex-center ring-2 rounded bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                  <FaEraser className="text-white" />
+                </button>
+                :
+                <button onClick={() => setDraw(false)}
+                  className="px-4 py-1 w-16 flex-center ring-2 rounded">
+                  <FaEraser />
+                </button>
+              }
+            </div>
+
 
             <div className="flex gap-4 items-center">
-              <p className="text-red-500 text-xl text-extrabold">R</p>
+              <p className="text-red-500 text-2xl text-extrabold">R</p>
               <input id="red" type="range" min="0" max="255" value={red} onChange={handleColorChange} />
             </div>
             <div className="flex gap-4 items-center">
-              <p className="text-green-500 text-xl text-extrabold">G</p>
+              <p className="text-green-500 text-2xl text-extrabold">G</p>
               <input id="green" type="range" min="0" max="255" value={green} onChange={handleColorChange} />
             </div>
             <div className="flex gap-4 items-center">
-              <p className="text-blue-500 text-xl text-extrabold">B</p>
+              <p className="text-blue-500 text-2xl text-extrabold">B</p>
               <input id="blue" type="range" min="0" max="255" value={blue} onChange={handleColorChange} />
             </div>
 
             <button onClick={handleClear}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              className="bg-blue-500 hover:bg-blue-700 text-white text-2xl font-bold py-2 px-4 rounded">
               Clear All
             </button>
           </div>
           <canvas width="800" height="600" ref={canvasRef}
-            className="border-2 rounded" />
+            className="border-2 rounded border-grey-900 " />
         </div>
-      </body>
+      </body >
     </>
   )
 }
